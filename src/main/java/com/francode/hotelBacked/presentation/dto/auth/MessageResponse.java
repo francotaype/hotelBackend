c124a265ae36c00c06c504389371f7f0c7b23b0c
FrancoTCC <i2317201@continental.edu.pe>
2025-02-13 13:30:13 -0600

    feat: add auth DTOs

---
+++ src/main/java/com/francode/hotelBacked/presentation/dto/auth/MessageResponse.java
@@ -0,0 +1,13 @@
+package com.francode.hotelBacked.presentation.dto.auth;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MessageResponse {
+    private String message;
+
+}
