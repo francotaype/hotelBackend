a49de0df451c2fb9ed8c5135567061eeac4688c1
FrancoTCC <i2317201@continental.edu.pe>
2025-02-28 17:14:42 -0600

    feat: add company seeder with validation check before creation

--- src/main/java/com/francode/hotelBackend/persistence/DataSeeders/DataSeeder.java
+++ src/main/java/com/francode/hotelBackend/persistence/DataSeeders/DataSeeder.java
@@ -1,8 +1,10 @@
 package com.francode.hotelBackend.persistence.DataSeeders;
 
+import com.francode.hotelBackend.domain.entity.Company;
 import com.francode.hotelBackend.domain.entity.ERole;
 import com.francode.hotelBackend.domain.entity.Role;
 import com.francode.hotelBackend.domain.entity.UserApp;
+import com.francode.hotelBackend.persistence.repository.JpaCompanyRepository;
 import com.francode.hotelBackend.persistence.repository.JpaUserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,17 +17,19 @@ import java.util.Set;
 public class DataSeeder implements CommandLineRunner {
 
     private final JpaUserRepository userRepository;
+    private final JpaCompanyRepository companyRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public DataSeeder(JpaUserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public DataSeeder(JpaUserRepository userRepository, JpaCompanyRepository companyRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.companyRepository = companyRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        // Crear usuario admin
         if (userRepository.findByEmail("admin@gmail.com").isEmpty()) {
-
             Role adminRole = new Role();
             adminRole.setName(ERole.ROLE_ADMIN);
 
@@ -40,6 +44,22 @@ public class DataSeeder implements CommandLineRunner {
             System.out.println("Usuario admin creado exitosamente.");
         } else {
             System.out.println("El usuario admin ya existe.");
+        }
+
+        // Crear empresa
+        if (companyRepository.findById(1L).isEmpty()) {
+            Company company = new Company();
+            company.setName("Formula 1");
+            company.setEmail("formula1@f1.com");
+            company.setRuc("12345678901");
+            company.setLocation("Ubicación de la empresa");
+            company.setPhone("963265986");
+            company.setLogoUrl("http://example.com/logo.png");
+
+            companyRepository.save(company);
+            System.out.println("Empresa creada exitosamente.");
+        } else {
+            System.out.println("La empresa ya existe.");
         }
     }
 }
