74131b9ce3fb76b275774980cced5242091e08db
FrancoTCC <i2317201@continental.edu.pe>
2025-02-28 15:32:02 -0600

    feat: update Employee repository to return Optional<Employee> for documentNumber search

--- src/main/java/com/francode/hotelBackend/persistence/repository/JpaEmployeeRepository.java
+++ src/main/java/com/francode/hotelBackend/persistence/repository/JpaEmployeeRepository.java
@@ -1,7 +1,11 @@
 package com.francode.hotelBackend.persistence.repository;
 
 import com.francode.hotelBackend.domain.entity.Employee;
-import com.francode.hotelBackend.persistence.generics.GenericRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface JpaEmployeeRepository extends GenericRepository<Employee, Long> {
+import java.util.Optional;
+
+public interface JpaEmployeeRepository extends JpaRepository<Employee, Long>, JpaSpecificationExecutor<Employee> {
+    Optional<Employee> findByDocumentNumber(String documentNumber);
 }
