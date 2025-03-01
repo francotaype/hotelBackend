f4743595f5a1744bfd919e8a6c2e6f4f7069434a
FrancoTCC <i2317201@continental.edu.pe>
2025-02-28 18:26:58 -0600

    feat: add JpaClientRepository for managing client

--- src/main/java/com/francode/hotelBackend/persistence/repository/JpaClientRepository.java
+++ src/main/java/com/francode/hotelBackend/persistence/repository/JpaClientRepository.java
@@ -3,5 +3,8 @@ package com.francode.hotelBackend.persistence.repository;
 import com.francode.hotelBackend.domain.entity.Client;
 import com.francode.hotelBackend.persistence.generics.GenericRepository;
 
+import java.util.Optional;
+
 public interface JpaClientRepository extends GenericRepository<Client, Long> {
+    Optional<Client> findByDocumentNumber(String documentNumber);
 }
